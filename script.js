345d3b9e1e6871cfb97e774cf72c696b630a339b
Karol Zajac <kicek1324@gmail.com>
2021-09-23 13:34:22 +0200

    update scene

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
-import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene()
+const canvas = document.querySelector('.webgl');
+const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-})
-
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 //Boilerplate code
 
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
-}
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(0,1,2)
-scene.add(camera)
+};
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 1000);
+camera.position.setZ(30);
+scene.add(camera);
 
 const renderer = new THREE.WebGL1Renderer({
     canvas: canvas
-})
+});
 
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-renderer.shadowMap.enabled = true
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+renderer.shadowMap.enabled = true;
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({
+    color: 0x2ed1ae
+});
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+
+
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate(){
-    requestAnimationFrame(animate)
-    renderer.render(scene,camera)
-}
+    requestAnimationFrame(animate);
 
-animate()
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    controls.update();
+
+    renderer.render(scene,camera);
+};
+
+animate();
